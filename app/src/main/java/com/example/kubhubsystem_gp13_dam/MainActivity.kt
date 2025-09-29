adac6b572f048bd075b45a50367fed2f83645b82
Matheus Lara <ma.delara@duocuc.cl>
2025-09-29 19:02:50 -0300

    2 commit

--- app/src/main/java/com/example/kubhubsystem_gp13_dam/MainActivity.kt
+++ app/src/main/java/com/example/kubhubsystem_gp13_dam/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.kubhubsystem_gp13_dam.ui.theme.HomeScreen
 import com.example.kubhubsystem_gp13_dam.ui.theme.KubHubSystem_gp13_DAMTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KubHubSystem_gp13_DAMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
